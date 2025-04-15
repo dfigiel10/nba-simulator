93fe19844087d8a29771dd7e6f4358db9ad80b41
Daniel Figiel <dfigiel10@gmail.com>
2025-04-15 14:18:00 -0400

    adding initial files for spring boot

---
+++ src/main/java/com/nba/nba_simulation/NbaSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nba_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba/nba_simulation/NbaSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nba_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
