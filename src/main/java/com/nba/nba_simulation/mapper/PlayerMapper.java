d5a99dc1292561a0e6f7311f5701e690ea253c2b
Daniel Figiel <dfigiel10@gmail.com>
2025-04-16 21:55:00 -0400

    Fixed player mapper

--- src/main/java/com/nba/nba_simulation/mapper/PlayerMapper.java
+++ src/main/java/com/nba/nba_simulation/mapper/PlayerMapper.java
@@ -2,11 +2,20 @@ package com.nba.nba_simulation.mapper;
 
 import com.nba.nba_simulation.dto.PlayerDto;
 import com.nba.nba_simulation.entity.Player;
+import com.nba.nba_simulation.entity.PlayerTeam;
+import com.nba.nba_simulation.entity.Team;
+import com.nba.nba_simulation.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class PlayerMapper {
-    public static Player mapToPlayer(PlayerDto playerDto) {
+    // @Autowired
+    // private static TeamRepository teamRepository;
+
+    public static Player mapToPlayer(PlayerDto playerDto, List<Team> teams) {
         //List<Team> teams =
         Player player = new Player();
         player.setFirstName(playerDto.getFirstName());
@@ -14,6 +23,16 @@ public class PlayerMapper {
         player.setId(playerDto.getId());
         player.setHeight(playerDto.getHeight());
         player.setWeight(playerDto.getWeight());
+
+        List<PlayerTeam> playerTeams = new ArrayList<>();
+
+        for (Team team : teams) {
+            PlayerTeam playerTeam = new PlayerTeam();
+            playerTeam.setPlayer(player);
+            playerTeam.setTeam(team);
+            playerTeams.add(playerTeam);
+        }
+        player.setTeams(playerTeams);
         return player;
     }
 
@@ -24,7 +43,11 @@ public class PlayerMapper {
         pdto.setLastName(player.getLastName());
         pdto.setHeight(player.getHeight());
         pdto.setWeight(player.getWeight());
-        // pdto.setTeams(player.getTeams());
+        List<Long> teamIds = new ArrayList<>();
+        for (PlayerTeam playerTeam : player.getTeams()) {
+            teamIds.add(playerTeam.getTeam().getId());
+        }
+        pdto.setTeamIds(teamIds);
         return pdto;
     }
 }
