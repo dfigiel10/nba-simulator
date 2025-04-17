a0ef44aab30320a185f421fa9b4f5f51f874f2bb
Daniel Figiel <dfigiel10@gmail.com>
2025-04-16 21:56:04 -0400

    Fixed PlayerServiceImpl to handle teamRepository

--- src/main/java/com/nba/nba_simulation/service/Implementation/PlayerServiceImpl.java
+++ src/main/java/com/nba/nba_simulation/service/Implementation/PlayerServiceImpl.java
@@ -2,12 +2,16 @@ package com.nba.nba_simulation.service.Implementation;
 
 import com.nba.nba_simulation.dto.PlayerDto;
 import com.nba.nba_simulation.entity.Player;
+import com.nba.nba_simulation.entity.Team;
 import com.nba.nba_simulation.mapper.PlayerMapper;
 import com.nba.nba_simulation.repository.PlayerRepository;
+import com.nba.nba_simulation.repository.TeamRepository;
 import com.nba.nba_simulation.service.PlayerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,9 +19,12 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Autowired
     private PlayerRepository playerRepository;
+    @Autowired
+    private TeamRepository teamRepository;
 
     public PlayerDto createPlayer(PlayerDto playerDto) {
-        Player player = PlayerMapper.mapToPlayer(playerDto);
+        List<Team> teams = teamRepository.findAllById(playerDto.getTeamIds());
+        Player player = PlayerMapper.mapToPlayer(playerDto, teams);
         Player savedPlayer = playerRepository.save(player);
         return PlayerMapper.mapToPlayerDto(savedPlayer);
     }
