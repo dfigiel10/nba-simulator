e5907ebd41ac7affac079384dac071b53cddb0a5
Daniel Figiel <dfigiel10@gmail.com>
2025-04-21 23:21:53 -0400

    Created GamePlayerStats entity to keep track of player stats

--- src/main/java/com/nba/nba_simulation/entity/GamePlayerStats.java
+++ src/main/java/com/nba/nba_simulation/entity/GamePlayerStats.java
@@ -1,2 +1,35 @@
-package com.nba.nba_simulation.entity;public class GamePlayerStats {
+package com.nba.nba_simulation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="gamePlayerStats")
+public class GamePlayerStats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // many gamePlayerStats have one game
+    @JoinColumn(name="game_id")
+    private Game game;
+
+    @ManyToOne // many gamePlayerStats have one player
+    @JoinColumn(name="player_id")
+    private Player player;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    private int points;
+    private int rebounds;
+    private int assists;
 }
